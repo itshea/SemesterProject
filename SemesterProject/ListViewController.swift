dcaa456287a2bd2e3fc0b08b76ff0d3b290da465
jenwei00 <115328679+jenwei00@users.noreply.github.com>
2022-10-21 23:29:01 -0500

    Add files via upload
    grocery list vc

---
+++ SemesterProject/ListViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ListViewController.swift
+//  SemesterProject
+//
+//  Created by Jennifer Wei on 10/21/22.
+//
+
+import UIKit
+
+class ListViewController: UIViewController {
+    // tableview with different groceries to buy
+    // search bar that looks through user's grocery list
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    // bar button add
+    // add button to add a grocery
+    // need database? to search grocery, another segue?
+    
+    // how to delete items from list? make checkmark?
+    
+}
