75a3c16e0cb347416143d7a41935e1628d4d0c11
itshea <ishea@utexas.edu>
2022-11-28 15:19:13 -0600

    some test changes

--- SemesterProject/SignUpViewController.swift
+++ SemesterProject/SignUpViewController.swift
@@ -9,6 +9,12 @@ import UIKit
 
 class SignUpViewController: UIViewController {
 
+    @IBOutlet weak var firstName: UITextField!
+    @IBOutlet weak var lastName: UITextField!
+    @IBOutlet weak var email: UITextField!
+    @IBOutlet weak var pass: UITextField!
+    @IBOutlet weak var passCon: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -16,14 +22,6 @@ class SignUpViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func signupButtonPressed(_ sender: Any) {
     }
-    */
-
 }
