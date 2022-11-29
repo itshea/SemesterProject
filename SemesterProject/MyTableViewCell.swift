dab37744da5f3f09c7e2d13413ae61e5d84b31e0
itshea <ishea@utexas.edu>
2022-11-29 14:56:06 -0600

    notification cell changes

--- SemesterProject/MyTableViewCell.swift
+++ SemesterProject/MyTableViewCell.swift
@@ -24,4 +24,7 @@ class MyTableViewCell: UITableViewCell {
         // Configure the view for the selected state
     }
 
+    // removes item from notification list
+    @IBAction func readButtonTouch(_ sender: Any) {
+    }
 }
