de1007449fc4fe99c09ffdf9ed45fade08d8718f
itshea <ishea@utexas.edu>
2022-12-02 11:01:02 -0600

    cosmetic and welcome animation

--- SemesterProject/ViewController.swift
+++ SemesterProject/ViewController.swift
@@ -9,12 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var logoButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
 
-
+    @IBAction func fadeOut(_ sender: Any) {
+        self.logoButton.alpha = 1.0
+        
+        UIView.animate(
+            withDuration: 3.0,
+            animations: {
+                self.logoButton.alpha = 0.0
+            }
+        )
+    }
+    
 }
 
